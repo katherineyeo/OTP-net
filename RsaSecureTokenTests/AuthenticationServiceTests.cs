240d84ca5d8edfee8d7b3a034ba354c559caadde
hatelove <jay.hatelove@gmail.com>
2018-07-28 13:06:50 +0800

    add nunit reference

--- RsaSecureTokenTests/AuthenticationServiceTests.cs
+++ RsaSecureTokenTests/AuthenticationServiceTests.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using RsaSecureToken;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using RsaSecureToken;
+using Assert = NUnit.Framework.Assert;
+
 namespace RsaSecureToken.Tests
 {
-    [TestClass()]
+    [TestFixture]
     public class AuthenticationServiceTests
     {
-        [TestMethod()]
+        [Test()]
         public void IsValidTest()
         {
             var target = new AuthenticationService();
